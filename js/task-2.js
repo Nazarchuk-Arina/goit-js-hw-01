fb5e7ba67cf9356c398f4fb4276bb852ac6102e9
Arina Nazarchuk <nazarchuk.arina@gmail.com>
2024-07-14 15:43:22 +0300

    .

--- js/task-2.js
+++ js/task-2.js
@@ -2,3 +2,6 @@ function getShippingMessage(country, price, deliveryFee) {
   const totalPrice = deliveryFee + price;
   return `Shipping to ${country} will cost ${totalPrice} credits`;
 }
+console.log(getShippingMessage('Australia', 120, 150));
+console.log(getShippingMessage('Canada', 100, 60));
+console.log(getShippingMessage('Germany', 80, 40));
